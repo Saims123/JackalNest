64039c32d961157a8a22bf9cee3305a93cad48ba
Steve Sim <stevesim@hotmail.com>
2019-04-10 15:38:38 +0100

    Student: Controller status code and messages

--- src/students/student-controller.ts
+++ src/students/student-controller.ts
@@ -2,64 +2,82 @@ import * as express from 'express';
 import { Student } from './student-interface';
 import studentModel from './student-model';
 class StudentController {
-	public path = '/student';
-	public router = express.Router();
-	private students = studentModel;
+  public path = '/student';
+  public router = express.Router();
+  private students = studentModel;
 
-	constructor() {
-		this.intializeRoutes();
-	}
+  constructor() {
+    this.intializeRoutes();
+  }
 
-	public intializeRoutes() {
-		this.router.get(`${this.path}/:id`, this.getStudentByID);
-		this.router.post(`${this.path}`, this.addNewStudent);
-		this.router.get(`${this.path}`, this.getAllStudents);
-		this.router.delete(`${this.path}/delete/:id`, this.deleteAStudent);
-	}
+  public intializeRoutes() {
+    this.router.get(`${this.path}/:id`, this.getStudentByID);
+    this.router.post(`${this.path}/`, this.addNewStudent);
+    this.router.get(`${this.path}`, this.getAllStudents);
+    this.router.delete(`${this.path}/:id`, this.deleteAStudent);
+  }
 
-	addNewStudent = async (request: express.Request, response: express.Response) => {
-		const st: Student = request.body;
-		const findStudent = await this.students.findOne({ uniqueID: st.uniqueID });
-		if (!findStudent) {
-			this.students.create({
-				uniqueID: st.uniqueID,
-				displayName: st.displayName,
-				email: st.email,
-				course: st.course
-			});
-			response.send('success', 201, st);
-		}
-		response.send('Unsucessful', 400);
-	};
+  addNewStudent = async (
+    request: express.Request,
+    response: express.Response
+  ) => {
+    const student: Student = request.body;
+    const findStudent = await this.students.findOne({
+      uniqueID: student.uniqueID
+    });
+    if (!findStudent) {
+      this.students.create({
+        uniqueID: student.uniqueID,
+        displayName: student.displayName,
+        email: student.email,
+        course: student.course
+      });
+      response
+        .status(200)
+        .send({ message: 'Successfully added student', student: student });
+    } else {
+      response.status(400).send('Student already exist');
+    }
+  };
+  getStudentByID = (request: express.Request, response: express.Response) => {
+    const id: number = request.params.id;
+    this.students.findOne({ uniqueID: id }).then(student => {
+      response.send(student);
+    });
+  };
 
-	getStudentByID = (request: express.Request, response: express.Response) => {
-		const id: number = request.params.id;
-		this.students.findOne({ uniqueID: id }).then(student => {
-			response.send(student);
-		});
-	};
+  getAllStudents = (request: express.Request, response: express.Response) => {
+    this.students.find().then(students => {
+      if (students) {
+        response.status(200).send(students);
+      } else {
+        response.status(404).send('Student Not found', students);
+      }
+    });
+  };
 
-	getAllStudents = (request: express.Request, response: express.Response) => {
-		this.students.find().then(students => {
-			response.send(students);
-		});
-	};
-
-	deleteAStudent = (request: express.Request, response: express.Response) => {
-		const id: number = request.params.id;
-		let d: any;
-		this.students
-			.findOne({ uniqueID: id })
-			.remove()
-			.exec()
-			.then(data => {
-				if (data) {
-					response.send(data);
-				} else {
-					response.send(404, 'Not deleted for unknown reason', data);
-				}
-			});
-	};
+  deleteAStudent = (request: express.Request, response: express.Response) => {
+    const id: number = request.params.id;
+    let d: any;
+    this.students
+      .findOne({ uniqueID: id })
+      .remove()
+      .exec()
+      .then(data => {
+        if (data) {
+          response
+            .status(200)
+            .send({ message: 'Successfully removed student', response: data });
+        } else {
+          response
+            .status(404)
+            .send({
+              message: 'Not deleted for unknown reason',
+              response: data
+            });
+        }
+      });
+  };
 }
 
 export default StudentController;
